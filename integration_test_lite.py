35c9f2805181bb5a310b62a2ce833efdd4a4a50a
ianbrown9475 <ian.brown9475@gmail.com>
2019-07-31 12:39:39 -0700

    WIP: Use async requests with local api

--- integration_test_lite.py
+++ integration_test_lite.py
@@ -1,7 +1,10 @@
-import requests
+import asyncio
 import json
 import sys
 import uuid
+
+import aiohttp
+import requests
 
 
 # Helper function to pretty print a list or dictionary
@@ -16,7 +19,7 @@ def get_access_token():
         'client_secret': config_json["client_secret"],
         'grant_type': 'client_credentials'
     }
-    request = requests.post(config_json["token_api"], data=post_data)
+    request = session.post(config_json["token_api"], data=post_data)
 
     return f'Bearer {request.json()["access_token"]}'
 
@@ -28,23 +31,25 @@ def basic_request(url, params, needs_access_token, verb="get"):
     if (needs_access_token):
         headers['Authorization'] = access_token
 
-    request = requests.request(verb, url, params=params, headers=headers)
+    request = session.request(
+        verb, url, params=params, headers=headers, ssl=False
+    )
     return request
 
 
 # Makes a call to each API and adds the API
 # to a list if the call wasn't successful
-def get_bad_apis():
+async def get_bad_apis():
     bad_apis = []
 
-    for endpoint in config_json["target_endpoints"]:
+    async def test_endpoint(endpoint):
         query_params = endpoint["query_params"]
         # Add random query parameter with random value to bypass caching
         query_params[uuid.uuid4().hex] = uuid.uuid4().hex
 
-        request = basic_request(
+        request = await basic_request(
             endpoint["base_url"], query_params, endpoint["needs_access_token"])
-        response_code = request.status_code
+        response_code = request.status
 
         allowed_response_codes = [200]
         if 'allow_400' in endpoint and endpoint['allow_400']:
@@ -55,23 +60,46 @@ def get_bad_apis():
             api_info["response_code"] = response_code
 
             try:
-                api_info["response_body"] = request.json()
+                api_info["response_body"] = await request.json()
             except ValueError as error:
                 api_info["response_body"] = str(error)
 
             bad_apis.append(api_info)
+            request.close()
+
+    tasks = []
+    for endpoint in config_json["target_endpoints"]:
+        tasks.append(test_endpoint(endpoint))
+
+    await asyncio.gather(*tasks)
 
     return bad_apis
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
+async def main():
     config_data_file = open(sys.argv[1])
+    global config_json
     config_json = json.load(config_data_file)
 
-    access_token = get_access_token()
-    bad_apis = get_bad_apis()
+    config_json['target_endpoints'] = config_json['target_endpoints'] * 10
+
+    global session
+    # session = requests.Session()
+    # session.verify = False
+    # session.auth = ('username', 'password')
+    auth = aiohttp.BasicAuth('username', 'password')
+    session = aiohttp.ClientSession(auth=auth)
+
+    global access_token
+    # access_token = get_access_token()
+    access_token = None
+    bad_apis = await get_bad_apis()
+    await session.close()
 
     if len(bad_apis) > 0:
         print("The following API(s) returned errors:")
         pretty_print(bad_apis)
         sys.exit(1)
+
+asyncio.run(main())
