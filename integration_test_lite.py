9970e42123f1cc582b2b1bd3060f8873f141b5eb
ianbrown9475 <ian.brown9475@gmail.com>
2019-07-31 17:48:29 -0700

    Refactor bad_response

--- integration_test_lite.py
+++ integration_test_lite.py
@@ -15,12 +15,6 @@ class IntegrationTestLite:
     def __init__(self):
         config_data_file = open(sys.argv[1])
         self.config_json = json.load(config_data_file)
-
-        # TODO remove
-        self.config_json['target_endpoints'] = (
-            self.config_json['target_endpoints'] * 10
-        )
-
         self.session = aiohttp.ClientSession()
 
     async def __aenter__(self):
@@ -54,46 +48,41 @@ class IntegrationTestLite:
         request = session.request(verb, url, params=params, headers=headers)
         return request
 
-    # Makes a call to each API and adds the API
-    # to a list if the call wasn't successful
+    async def bad_response(self, endpoint):
+        query_params = endpoint["query_params"]
+        # Add random query parameter with random value to bypass caching
+        query_params[uuid.uuid4().hex] = uuid.uuid4().hex
+
+        request = await self.basic_request(
+            endpoint["base_url"],
+            query_params,
+            endpoint["needs_access_token"]
+        )
+        response_code = request.status
+
+        allowed_response_codes = [200]
+        if 'allow_400' in endpoint and endpoint['allow_400']:
+            allowed_response_codes.append(400)
+
+        if response_code not in allowed_response_codes:
+            api_info = endpoint
+            api_info["response_code"] = response_code
+
+            try:
+                api_info["response_body"] = await request.json()
+            except ValueError as error:
+                api_info["response_body"] = str(error)
+            return api_info
+
     async def get_bad_apis(self):
-        bad_apis = []
+        endpoints = self.config_json['target_endpoints']
 
-        async def test_endpoint(endpoint):
-            query_params = endpoint["query_params"]
-            # Add random query parameter with random value to bypass caching
-            query_params[uuid.uuid4().hex] = uuid.uuid4().hex
-
-            request = await self.basic_request(
-                endpoint["base_url"],
-                query_params,
-                endpoint["needs_access_token"]
-            )
-            response_code = request.status
-
-            allowed_response_codes = [200]
-            if 'allow_400' in endpoint and endpoint['allow_400']:
-                allowed_response_codes.append(400)
-
-            if response_code not in allowed_response_codes:
-                api_info = endpoint
-                api_info["response_code"] = response_code
-
-                try:
-                    api_info["response_body"] = await request.json()
-                except ValueError as error:
-                    api_info["response_body"] = str(error)
-
-                bad_apis.append(api_info)
-                # request.close()
-
-        tasks = []
-        for endpoint in self.config_json["target_endpoints"]:
-            tasks.append(test_endpoint(endpoint))
-
-        await asyncio.gather(*tasks)
-
-        return bad_apis
+        # Execute all tests in parallel
+        results = await asyncio.gather(
+            *[self.bad_response(endpoint) for endpoint in endpoints]
+        )
+        # Return all results that weren't None
+        return [result for result in results if result]
 
 
 async def main():
@@ -101,9 +90,9 @@ async def main():
         await integration_test_lite.set_access_token()
         bad_apis = await integration_test_lite.get_bad_apis()
 
-    if len(bad_apis) > 0:
-        print("The following API(s) returned errors:")
-        pretty_print(bad_apis)
-        sys.exit(1)
+        if len(bad_apis) > 0:
+            print("The following API(s) returned errors:")
+            pretty_print(bad_apis)
+            sys.exit(1)
 
 asyncio.run(main())
