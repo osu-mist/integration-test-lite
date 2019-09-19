7cc6320b646578310ac4b8a5aa585c793885013e
ianbrown9475 <ian.brown9475@gmail.com>
2019-09-19 11:57:37 -0700

    Use new ClientSession for every request
    While using a single persistent session can have better performance,
    this was causing timeouts

--- integration_test_lite.py
+++ integration_test_lite.py
@@ -20,19 +20,14 @@ class IntegrationTestLite:
     def __init__(self):
         config_data_file = open(sys.argv[1])
         self.config_json = json.load(config_data_file)
-        self.timeout = self.config_json['request_timeout']
-        timeout = aiohttp.ClientTimeout(total=self.timeout)
-        self.session = aiohttp.ClientSession(timeout=timeout)
+        self.total = self.config_json['request_timeout']
+        self.timeout = aiohttp.ClientTimeout(total=self.total)
 
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(self, *excinfo):
-        await self.session.close()
+    def get_session(self):
+        return aiohttp.ClientSession(timeout=self.timeout)
 
     async def set_access_token(self):
         """Helper function to set the access token"""
-        session = self.session
         config_json = self.config_json
         post_data = {
             'client_id': config_json['client_id'],
@@ -40,13 +35,24 @@ class IntegrationTestLite:
             'grant_type': 'client_credentials'
         }
 
-        response = await session.post(config_json['token_api'], data=post_data)
-        response_body = await response.json()
-        self.access_token = f"Bearer {response_body['access_token']}"
+        async with self.get_session() as session:
+            async with (
+                session.post(config_json['token_api'], data=post_data)
+            ) as response:
+                response_body = await response.json()
+                self.access_token = f"Bearer {response_body['access_token']}"
 
-    def basic_request(self, url, params, needs_access_token, method='get'):
+    def basic_request(
+            self,
+            session,
+            url,
+            params,
+            needs_access_token,
+            method='get'
+    ):
         """Basic http request with url and query parameters
 
+        :param session aiohttp.ClientSession: Session for making request
         :param str url: Request url
         :param dict params: Request params
         :param bool needs_access_token: True if request required an access
@@ -54,8 +60,6 @@ class IntegrationTestLite:
         :param str method: HTTP request method
         :returns: An awaitable HTTP response
         """
-        session = self.session
-
         headers = {}
         if needs_access_token:
             headers['Authorization'] = self.access_token
@@ -81,11 +85,18 @@ class IntegrationTestLite:
         if re.match(textbooks_url_regex, url):
             try:
                 terms_url = 'https://osu.verbacompare.com/compare/courses'
-                terms_response = await self.basic_request(terms_url, {}, False)
-                terms_json = await terms_response.json()
-                query_params['academicYear'], query_params['term'] = (
-                    terms_json[0]['id'].split('-')
-                )
+                async with self.get_session() as session:
+                    async with  self.basic_request(
+                        session,
+                        terms_url,
+                        {},
+                        False
+                    ) as terms_response:
+                        terms_json = await terms_response.json()
+                        query_params['academicYear'], query_params['term'] = (
+                            terms_json[0]['id'].split('-')
+                        )
+                        print(terms_json)
             except Exception as error:
                 api_info['error'] = (
                     f'Exception when querying textbooks terms: {str(error)}'
@@ -93,30 +104,34 @@ class IntegrationTestLite:
                 return api_info
 
         try:
-            response = await self.basic_request(
-                url,
-                query_params,
-                endpoint['needs_access_token']
-            )
+            async with self.get_session() as session:
+                async with self.basic_request(
+                    session,
+                    url,
+                    query_params,
+                    endpoint['needs_access_token']
+                ) as response:
+
+                    response_code = response.status
+                    if response_code != 200:
+                        api_info['response_code'] = response_code
+
+                        try:
+                            api_info['response_body'] = await response.json()
+                        except Exception as error:
+                            api_info['response_body'] = {
+                                'raw': await response.text(),
+                                'error': str(error)
+                            }
+                        return api_info
+                    else:
+                        query_param_string = urlencode(query_params)
+                        print(f'    [{response_code}] {url}?'
+                              '{query_param_string}')
+
         except asyncio.TimeoutError:
-            api_info['error'] = f'Timed out after {self.timeout} second(s)'
+            api_info['error'] = f'Timed out after {self.total} second(s)'
             return api_info
-        response_code = response.status
-
-        if response_code != 200:
-            api_info['response_code'] = response_code
-
-            try:
-                api_info['response_body'] = await response.json()
-            except Exception as error:
-                api_info['response_body'] = {
-                    'raw': await response.text(),
-                    'error': str(error)
-                }
-            return api_info
-        else:
-            query_param_string = urlencode(query_params)
-            print(f'    [{response_code}] {url}?{query_param_string}')
 
     async def get_bad_apis(self):
         """Tests all endpoints and returns a list of errors
@@ -137,13 +152,13 @@ class IntegrationTestLite:
 
 
 async def main():
-    async with IntegrationTestLite() as integration_test_lite:
-        await integration_test_lite.set_access_token()
-        bad_apis = await integration_test_lite.get_bad_apis()
+    integration_test_lite = IntegrationTestLite()
+    await integration_test_lite.set_access_token()
+    bad_apis = await integration_test_lite.get_bad_apis()
 
-        if bad_apis:
-            print('The following API(s) returned errors:')
-            pretty_print(bad_apis)
-            sys.exit(1)
+    if bad_apis:
+        print('The following API(s) returned errors:')
+        pretty_print(bad_apis)
+        sys.exit(1)
 
 asyncio.run(main())
